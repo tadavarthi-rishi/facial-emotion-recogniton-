957e5e01dad2144166a75abe22ecac05cd8066f2
tadavarthi-rishi <rtadavar@asu.edu>
2024-04-01 09:06:40 -0700

    testing2

--- main.py
+++ main.py
@@ -19,5 +19,8 @@ def video_feed():
     return Response(gen(VideoCamera()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', debug=True)
+
+if __name__ == "__main__":
+    app.run(debug=True)
