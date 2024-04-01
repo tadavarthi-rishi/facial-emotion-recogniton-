47609646dfc4704eccdd78906c193b88b486d6f2
tadavarthi-rishi <rtadavar@asu.edu>
2024-04-01 10:13:53 -0700

    testing2

--- camera.py
+++ camera.py
@@ -13,11 +13,32 @@ class VideoCamera(object):
     def __del__(self):
         self.video.release()
 
-    # returns camera frames along with bounding boxes and predictions
+    # # returns camera frames along with bounding boxes and predictions
+    # def get_frame(self):
+    #     _, fr = self.video.read()
+    #     gray_fr = cv2.cvtColor(fr, cv2.COLOR_BGR2GRAY)
+    #     faces = facec.detectMultiScale(gray_fr, 1.3, 5)
+
+    #     for (x, y, w, h) in faces:
+    #         fc = gray_fr[y:y+h, x:x+w]
+
+    #         roi = cv2.resize(fc, (48, 48))
+    #         pred = model.predict_emotion(roi[np.newaxis, :, :, np.newaxis])
+
+    #         cv2.putText(fr, pred, (x, y), font, 1, (255, 255, 0), 2)
+    #         cv2.rectangle(fr,(x,y),(x+w,y+h),(255,0,0),2)
+
+    #     _, jpeg = cv2.imencode('.jpg', fr)
+    #     return jpeg.tobytes()
     def get_frame(self):
         _, fr = self.video.read()
         gray_fr = cv2.cvtColor(fr, cv2.COLOR_BGR2GRAY)
         faces = facec.detectMultiScale(gray_fr, 1.3, 5)
+
+        if len(faces) == 0:
+        # No faces detected, return the original frame
+            _, jpeg = cv2.imencode('.jpg', fr)
+            return jpeg.tobytes()
 
         for (x, y, w, h) in faces:
             fc = gray_fr[y:y+h, x:x+w]
@@ -30,3 +51,5 @@ class VideoCamera(object):
 
         _, jpeg = cv2.imencode('.jpg', fr)
         return jpeg.tobytes()
+
+        
